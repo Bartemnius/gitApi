65ebe1eb99582b1985ba2d728dfd4f57f17644c5
Bartemnius <wojtekdluski@wp.pl>
2024-02-22 12:12:07 +0100

    first commit

---
+++ src/main/java/com/project/gitApi/GitRepoInfoApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.gitApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRepoInfoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRepoInfoApiApplication.class, args);
+	}
+
+}
